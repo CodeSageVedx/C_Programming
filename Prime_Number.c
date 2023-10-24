dd03b805ad60c4b3ee9639c48a05bb1d0578e9fd
Vedant Kanoje <141636261+CodeSageVedx@users.noreply.github.com>
2023-10-24 11:16:32 +0530

    Update Prime_Number.c

--- Prime_Number.c
+++ Prime_Number.c
@@ -3,18 +3,18 @@
 int main(){
   int n;
   int i;
-  int flask = 1;
+  int flag = 1;
   printf("The number to check :\n ");
   scanf("%d",&n);
   for (i = 2 ;i < n; i++ ){
     if (n%i== 0){
-      flask = 0;
+      flag = 0;
       break;
     }
   }   
-  if (flask == 0){
+  if (flag == 0){
     printf("The number is not prime.\n");
-  }else if(flask == 1){
+  }else if(flag == 1){
     printf("The Number is Prime.");
   }
 
